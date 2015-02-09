0e384543f976fd4ccc6e8c52acc9eba9d20518d6
Giovanni Bryden <giovannibryden@gmail.com>
2015-02-09 11:08:08 -0800

    Changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'static/home'
+  
+  root 'static#home'
 
-  get 'static/about'
+  get 'about' => 'static#about'
 
-  get 'static/help'
+  get 'help' => 'static#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
