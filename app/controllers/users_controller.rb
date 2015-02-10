98442c1358043006e1858f267484b0b5690898ac
Giovanni Bryden <giovannibryden@gmail.com>
2015-02-10 10:20:23 -0800

    Implemented users model

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+  
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:notice] = 'Successfully created an account!'
+  		redirect_to root_path
+  	else
+  		flash[:notice] = 'Oops!'
+  		render 'new'
+  end
+
+
+private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+	end
+
+end
