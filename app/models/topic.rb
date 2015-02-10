506d5aeb7ab079c1005903799532a02e09b163b2
Giovanni Bryden <giovannibryden@gmail.com>
2015-02-10 09:38:14 -0800

    Validate length of body and topic.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ActiveRecord::Base
 	validates_length_of :body, :within => 4..750
-	validates_presence_of :topic, length: {maximum: 30}, uniqueness: {case_sensitive: false}
+	validates_length_of :topic, :within => 4..30
+	validates_presence_of :topic, uniqueness: {case_sensitive: false}
 end
