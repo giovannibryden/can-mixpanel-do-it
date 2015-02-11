e6df610fce10441529d54facb4ad4d6d9c90804f
Giovanni Bryden <giovannibryden@gmail.com>
2015-02-10 16:37:45 -0800

    Can create comments, but they're not being associated the the topic under which they were created.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210221847) do
+ActiveRecord::Schema.define(version: 20150211003318) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150210221847) do
     t.integer  "topics_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
 
   add_index "comments", ["topics_id"], name: "index_comments_on_topics_id"
