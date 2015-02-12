cf2ebbc8cd6ff08d88206784f06d393b5b9d84c8
Giovanni Bryden <giovannibryden@gmail.com>
2015-02-11 17:27:48 -0800

    Added header links for ease of navigation. Changes to sessions controller.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-  	user = user.find_by(:email params[:session][:email].downcase)
+  	user = User.find_by(email: params[:session][:email].downcase)
 
   	# Check to see if user exists AND that they can authenticate with the password
   	if user && user.authenticate(params[:session][:password])
-  
+  		flash[:success] = "Whoa. It worked!"
+  		redirect_to root_path
+  	else
+  		flash[:notice] = "Something's wrong!"
   	end
   end	
 
