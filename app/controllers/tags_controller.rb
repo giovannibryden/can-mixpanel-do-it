190b9c4759a58b50e79fc0f78363790d1c6c6fbe
Giovanni Bryden <giovannibryden@gmail.com>
2015-02-18 14:40:13 -0800

    Changes to tags controller, added tag partial.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,15 @@
 class TagsController < ApplicationController
+	def create
+		@tag = Tag.new(tag_params)
+		if @tag.save
+			flash[:success] = "Successfully created a tag!"
+		else
+			flash[:notice] = "You suck at creating tags."
+	end
+
+private
+
+def tag_params
+	params.require(:tag).permit(:name,:topic_id)
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216221322) do
+ActiveRecord::Schema.define(version: 20150218223354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20150216221322) do
   end
 
   add_index "comments", ["topics_id"], name: "index_comments_on_topics_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "topic"
