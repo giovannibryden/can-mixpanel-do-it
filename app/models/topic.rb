b821defb0632a8d0f9d9554f8ed9cfa032ff3ef5
Giovanni Bryden <giovannibryden@gmail.com>
2015-03-27 11:15:49 -0700

    More styles.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ActiveRecord::Base
-	validates_length_of :body, :within => 4..750
+	validates_length_of :body, :within => 1..750
 	validates :topic, uniqueness: {case_sensitive: false}, length: { maximum: 750 }
 	has_many :comments, :dependent => :destroy
 	has_many :tags, :dependent => :destroy
